48cb5ce12581b6ac129d6a90f15097abe41b482e
Renan Soares <rjrenan77@gmail.com>
2019-07-23 16:34:22 -0300

    projeto completo

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Routes from './routes';
+
+import GlobalStyle from './syles/global';
+
+function App() {
+  return (
+    <>
+      <Routes />
+      <GlobalStyle />
+    </>
+  );
+}
+
+export default App;
